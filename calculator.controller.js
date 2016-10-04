aaab5cd546a93c7b9d1c7d78221a5be5ed847daa
Rivu Khoda <rivu.khoda@gmail.com>
2016-10-03 22:37:01 -0400

    Displays equation

--- calculator.controller.js
+++ calculator.controller.js
@@ -1,5 +1,34 @@
 angular
-    .module
+    .module('app')
     .controller('CalculatorController', CalculatorController);
 
-function CalculatorController() {}
+function CalculatorController() {
+
+    var vm = this;
+    vm.equation = "0";
+    var enteredFirstInput = false;
+
+    vm.append = function (value) {
+        if (!enteredFirstInput) {
+            vm.equation = "";
+            enteredFirstInput = true;
+        }
+        if (value === "+" || value === "-" || value === "/" || value === "*") {
+            vm.equation += " " + value + " ";
+        }
+        else {
+            vm.equation += value;
+        }
+
+    }
+
+    vm.clearAll = function () {
+        vm.equation = "0";
+        enteredFirstInput = false;
+    }
+
+    vm.calculate = function () {
+
+    }
+
+}
