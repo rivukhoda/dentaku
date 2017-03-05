91918a0f507d9f386d89219daa4ce015bfb2bfff
Rivu Khoda <rivu.khoda@gmail.com>
2017-03-04 19:58:45 -0500

    Configuring code coverage stats

--- my.conf.js
+++ my.conf.js
@@ -31,13 +31,21 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'calculator.controller.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
+
+
+    coverageReporter: {
+      type: 'lcovonly',
+      dir: 'coverage/',
+      subdir: '.'
+    },
 
 
     // web server port
